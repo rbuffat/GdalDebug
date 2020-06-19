7e22256b33c87cecbc83b103556bb10394b4f1e8
René Buffat <buffat@gmail.com>
2020-06-19 21:37:34 +0200

    update test

--- tests/test_gdal_import.py
+++ tests/test_gdal_import.py
@@ -1,9 +1,11 @@
 import pytest
+import os
 
 
 def test_import():
     from osgeo import ogr
     from osgeo import gdal
 
-    print(gdal.__version__) 
-    assert True
+    print(os.environ['GDALVERSION'], gdal.__version__)
+    assert os.environ['GDALVERSION'] == gdal.__version__
+
